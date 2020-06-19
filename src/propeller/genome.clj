ef6899d690459c456c4aa64d53c037c29aa08827
Mahran-Yousef <mahran.yousef2@gmail.com>
2020-06-19 11:12:19 -0400

    Using lexicase in the demo and reformatting

--- src/propeller/genome.clj
+++ src/propeller/genome.clj
@@ -4,16 +4,16 @@
 (defn push-from-plushy
   "Returns the Push program expressed by the given plushy representation."
   [plushy]
-  (let [opener? #(and (vector? %) (= (first %) 'open))] ;; [open <n>] marks opens
-    (loop [push () ;; iteratively build the Push program from the plushy
+  (let [opener? #(and (vector? %) (= (first %) 'open))]     ;; [open <n>] marks opens
+    (loop [push ()                                          ;; iteratively build the Push program from the plushy
            plushy (mapcat #(if-let [n (get opens %)] [% ['open n]] [%]) plushy)]
-      (if (empty? plushy)       ;; maybe we're done?
-        (if (some opener? push) ;; done with plushy, but unclosed open
-          (recur push '(close)) ;; recur with one more close
-          push)                 ;; otherwise, really done, return push
+      (if (empty? plushy)                                   ;; maybe we're done?
+        (if (some opener? push)                             ;; done with plushy, but unclosed open
+          (recur push '(close))                             ;; recur with one more close
+          push)                                             ;; otherwise, really done, return push
         (let [i (first plushy)]
           (if (= i 'close)
-            (if (some opener? push) ;; process a close when there's an open
+            (if (some opener? push)                         ;; process a close when there's an open
               (recur (let [post-open (reverse (take-while (comp not opener?)
                                                           (reverse push)))
                            open-index (- (count push) (count post-open) 1)
@@ -23,8 +23,8 @@
                          (concat pre-open [post-open])
                          (concat pre-open [post-open ['open (dec num-open)]])))
                      (rest plushy))
-              (recur push (rest plushy))) ;; unmatched close, ignore
-            (recur (concat push [i]) (rest plushy)))))))) ;; anything else
+              (recur push (rest plushy)))                   ;; unmatched close, ignore
+            (recur (concat push [i]) (rest plushy))))))))   ;; anything else
 
 (defn make-random-plushy
   "Creates and returns a new plushy."
