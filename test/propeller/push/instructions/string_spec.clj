d73b7c6117c3eabf4bdaf0dfcf3cab113f979943
Erik Rauer <eriktr22@gmail.com>
2021-03-16 22:18:37 -0500

    Add test for `string/concat`
    This adds a test for the `string/concat` instruction

--- test/propeller/push/instructions/string_spec.clj
+++ test/propeller/push/instructions/string_spec.clj
@@ -25,3 +25,19 @@
                 (check-butlast s)))
 
 
+;; string/concat
+
+(defn check-concat
+  [value1 value2]
+  (let [start-state (-> state/empty-state
+                        (state/push-to-stack :string value1)
+                        (state/push-to-stack :string value2))
+        end-state ((:string_concat @core/instruction-table) start-state)
+        expected-result (str value1 value2)]
+    (= expected-result
+       (state/peek-stack end-state :string))))
+  
+(defspec concat-spec 100
+  (prop/for-all [s1 gen/string
+                 s2 gen/string]
+                (check-concat s1 s2)))
