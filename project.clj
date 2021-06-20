e6a0b05e8e387e21da913bafd27aca1a16ea5dce
Tom Helmuth <thelmuth@hamilton.edu>
2021-06-19 22:55:17 -0400

    Added example for how to use datasets for PSB2.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/test.check "1.1.0"]]
+                 [org.clojure/test.check "1.1.0"]
+                 [net.clojars.schneau/psb2 "1.0.0"]]
   :main ^:skip-aot propeller.core
   :repl-options {:init-ns propeller.core})
