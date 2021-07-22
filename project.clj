995ed3b47d805934de991d64f5303b0871caef25
Lee Spector <lspector@hampshire.edu>
2021-07-21 23:32:36 -0400

    Update project.clj for clojars deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject propeller "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject net.clojars.lspector/propeller "0.2.0"
+  :description "Yet another Push-based genetic programming system in Clojure."
+  :url "https://github.com/lspector/propeller"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
